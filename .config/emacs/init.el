10abe6a16f97bcb6f45d720d0a3c27fc71935b73
Lawrence Logoh <lawrencel.logoh@gmail.com>
2022-02-27 23:45:51 +0000

    Add emacs and sxhkd config

---
+++ .config/emacs/init.el
@@ -0,0 +1,14 @@
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(markdown-mode rust-mode haskell-mode python-mode reformatter modus-themes emmet-mode eglot go-mode org-static-blog htmlize fira-code-mode pomodoro company golint magit use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
