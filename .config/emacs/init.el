94f390af43630d6b49ee9dc88dd540a28c9c53b9
Lawrence Logoh <lawrence.logoh@gmail.com>
2022-05-01 21:45:07 +0000

    emacs config fixes

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,11 +1,12 @@
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(org-babel-load-file (expand-file-name "personal.org" user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ox-reveal org-tree-slide use-package rust-mode reformatter python-mode pydoc modus-themes markdown-mode magit htmlize haskell-mode go-mode emmet-mode elcord eglot company)))
+   '(python-black ox-reveal org-tree-slide use-package rust-mode reformatter python-mode pydoc modus-themes markdown-mode magit htmlize haskell-mode go-mode emmet-mode elcord eglot company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
